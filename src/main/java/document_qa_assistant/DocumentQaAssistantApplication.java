d96d103ede60f049ce9d6322c3a62535bb81a5bb
Subhankar <guchaitsubhankar0@gmail.com>
2026-08-13 16:43:03 +0530

    chore: initialize Spring Boot project

---
+++ src/main/java/document_qa_assistant/DocumentQaAssistantApplication.java
@@ -0,0 +1,13 @@
+package document_qa_assistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentQaAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentQaAssistantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/document_qa_assistant/DocumentQaAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package document_qa_assistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentQaAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
